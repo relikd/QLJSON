ab7cee50ca6c1616e4cae06fa151074788f84eeb
relikd <info@relikd.de>
2026-04-20 10:29:33 +0200

    ref: window size restore

--- App/Document.swift
+++ App/Document.swift
@@ -9,23 +9,36 @@ class Document: NSDocument, NSWindowDelegate {
 	override func makeWindowControllers() {
 		let window = NSWindow(contentViewController: web)
 		addWindowController(NSWindowController(window: window))
-		let w = UserDefaults.standard.integer(forKey: "winSizeW")
-		let h = UserDefaults.standard.integer(forKey: "winSizeH")
-		window.setContentSize(NSSize(width: w > 0 ? w : 800, height: h > 0 ? h : 600))
+		restoreWindowSize(window)
 		window.makeKeyAndOrderFront(nil)
 		window.delegate = self
 	}
 	
 	func windowDidResize(_ notification: Notification) {
-		let sz = (notification.object as! NSWindow).frame.size
-		UserDefaults.standard.set(Int(sz.width), forKey: "winSizeW")
-		UserDefaults.standard.set(Int(sz.height), forKey: "winSizeH")
+		persistWindowSize(notification.object as! NSWindow)
 	}
 	
 	override nonisolated func read(from url: URL, ofType typeName: String) throws {
 		try MainActor.assumeIsolated {
 			try web.load(fromFile: url)
 			try watchForChanges(url)
+		}
+	}
+	
+	// MARK: - Window resize
+	
+	/// Save current window size to user-defaults
+	func persistWindowSize(_ win: NSWindow) {
+		UserDefaults.standard.set(Int(win.frame.width), forKey: "winSizeW")
+		UserDefaults.standard.set(Int(win.frame.height), forKey: "winSizeH")
+	}
+	
+	/// Restore previous window size
+	func restoreWindowSize(_ win: NSWindow) {
+		let w = UserDefaults.standard.integer(forKey: "winSizeW")
+		let h = UserDefaults.standard.integer(forKey: "winSizeH")
+		if w > 0 && h > 0 {
+			win.setFrame(CGRect(origin: win.frame.origin, size: .init(width: w, height: h)), display: true)
 		}
 	}
 	
