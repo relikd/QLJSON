c9228c7a95d383c4859ae6a6a52d5cc6e0eb7527
relikd <info@relikd.de>
2026-04-22 17:54:04 +0200

    ref: WebViewer

--- Common/WebViewer.swift
+++ Common/WebViewer.swift
@@ -1,20 +1,9 @@
 import WebKit
 
-func bundleFile(filename: String, ext: String) throws -> String {
-	if let appSupport = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-		let override = appSupport.appendingPathComponent(filename + "." + ext)
-		if FileManager.default.fileExists(atPath: override.path) {
-			return try String(contentsOfFile: override.path, encoding: .utf8)
-		}
-	}
-	// else, load bundle file
-	let path = Bundle.main.path(forResource: filename, ofType: ext)
-	return try String(contentsOfFile: path!, encoding: .utf8)
-}
-
 class WebViewer: NSViewController {
 	// sure, we could use `WKWebView`, but that requires the `com.apple.security.network.client` entitlement
 	let web = WebView()
+	var url: URL? = nil
 	
 	override func loadView() {
 		self.view = NSView(frame: NSMakeRect(0, 0, 800, 600))
@@ -27,40 +16,67 @@ class WebViewer: NSViewController {
 		self.view.addSubview(self.web)
 	}
 	
-	func load(html: String) {
-		self.web.mainFrame.loadHTMLString(html, baseURL: nil)  // WebView
-		//self.view.loadHTMLString(html, baseURL: nil)  // WKWebView
+	func load(fromFile url: URL) throws {
+		self.url = url
+		try self.reload()
 	}
 	
-	func load(fromFile: URL) throws {
-		let size = try FileManager.default.attributesOfItem(atPath: fromFile.path)[.size] as! NSNumber
-		guard size.uint64Value < 1_000_000 else {
-			self.load(html: """
-<!DOCTYPE html>
-<html>
-<head>
- <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
- <style>\(try bundleFile(filename: "style", ext: "css"))</style>
-</head>
-<body><p class="error">File too large</p></body>
-</html>
-""")
+	func reload() throws {
+		guard let url else {
 			return
 		}
+		let html = _html(try _body(url))
+		self.web.mainFrame.loadHTMLString(html, baseURL: nil) // WebView
+		//self.view.loadHTMLString(html, baseURL: nil) // WKWebView
+	}
+	
+	// MARK: - Content
+	
+	private func _body(_ url: URL) throws -> String {
+		let size = try? FileManager.default.attributesOfItem(atPath: url.path)[.size] as? NSNumber
+		// supports files up to 1MB (else the html+js will take too long to process)
+		guard (size?.uint64Value ?? 0) < 1_000_000 else {
+			return """
+<body><p class="error">File too large</p></body>
+"""
+		}
 		let scrollTo = web.mainFrame.domDocument.documentElement.scrollTop
-		let jsonFile = try String(contentsOf: fromFile, encoding: .utf8)
-		self.load(html: """
+		let jsonFile = try String(contentsOf: url, encoding: .utf8)
+		return """
+<body onload="init(\(scrollTo))">
+<script id="json" type="application/json">\(jsonFile)</script>
+</body>
+"""
+	}
+	
+	private func _html(_ body: String) -> String {
+		"""
 <!DOCTYPE html>
 <html>
 <head>
  <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
- <style>\(try bundleFile(filename: "style", ext: "css"))</style>
- <script>\(try bundleFile(filename: "script", ext: "js"))</script>
+ <style>\(bundleFile(filename: "style", ext: "css"))</style>
+ <script>\(bundleFile(filename: "script", ext: "js"))</script>
 </head>
-<body onload="init(\(scrollTo))">
- <script id="json" type="application/json">\(jsonFile)</script>
-</body>
+\(body)
 </html>
-""")
+"""
 	}
+}
+
+// MARK: - Extensions
+
+extension URL {
+	/// Folder for user modified html templates
+	static let UserModDir: URL? =
+		FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+}
+
+/// Load resource file either from user documents dir (if exists) or app bundle (default).
+func bundleFile(filename: String, ext: String) -> URL {
+	if let userFile = URL.UserModDir?.appendingPathComponent(filename + "." + ext, isDirectory: false),
+	   FileManager.default.fileExists(atPath: userFile.path) {
+		return userFile
+	}
+	return Bundle.main.url(forResource: filename, withExtension: ext)!
 }
